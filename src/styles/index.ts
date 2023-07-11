bf3ad75e7c4c2ae55c4ccb0310c6d2222e6eea05
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-12 01:13:59 +0800

    styling

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { theme, white } from "../assets/colors";
+import { black, theme, white } from "../assets/colors";
 
 export const styles = StyleSheet.create({
     container:{
@@ -9,4 +9,67 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: white.main
     },
+    h1: {
+        textAlign: 'left',
+        fontSize: 25,
+        fontFamily: 'Montserrat-Bold',
+        color: black.main,
+        width: '75%',
+    },
+    h4: {
+        fontSize: 17,
+        fontFamily: 'Montserrat-Medium',
+        marginTop: 5
+    },
+    h1container: {
+        width: '90%', 
+        justifyContent: 'center', 
+        alignItems: 'flex-start',
+        marginBottom: 15,
+    },
+    inputfield: {
+        width: '85%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        color: black.main,
+        fontSize: 17,
+        fontFamily: 'Montserrat-Medium',
+        paddingLeft: 0
+    },
+    joinasfield: {
+        width: '78%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        color: black.main,
+        fontSize: 17,
+        fontFamily: 'Montserrat-Medium',
+        paddingLeft: 5
+    },
+    joinastext: {
+        color: black.main,
+        fontSize: 17,
+        fontFamily: 'Montserrat-Regular',
+    },
+    inputcontainer: {
+        width: '90%',
+        height: 60,
+        backgroundColor: white.main,
+        marginVertical: 10,
+        borderWidth: 2,
+        borderColor: black.B005,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    createaccount: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '85%',
+        height: 55,
+        borderRadius: 15,
+    }
 })
