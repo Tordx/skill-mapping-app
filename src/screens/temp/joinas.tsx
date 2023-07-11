5769a10792b522a3091c13cfad6a994a2a73ae2c
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-12 01:13:28 +0800

    join as created

--- src/screens/temp/joinas.tsx
+++ src/screens/temp/joinas.tsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import { useFocusEffect } from '@react-navigation/native'
+import React, { useState } from 'react'
+import { useFocusEffect, useNavigation,  } from '@react-navigation/native'
 import { styles } from '../../styles'
-
-const {ImageBackground, Text, StatusBar, View} = require('react-native')
+import {  JoinasButton, LogButton, TextButton } from '../../global/partials/buttons'
+import { black, theme } from '../../assets/colors'
+import {ImageBackground, Text, StatusBar, View, ToastAndroid} from 'react-native'
 const background = require('../../assets/images/background.png')
-type Props = {}
 
-const Joinas: React.FC = (props: Props) => {
+type Props = {
 
 
+}
+
+const Joinas: React.FC<Props> = (props) => {
+
+  const [usertype, setusertype] = useState('');
+  const navigation = useNavigation();
+
+  const routechecking = () => {
+    if(usertype === 'employer') {
+      navigation.navigate('Employercreate' as never)
+    } else if (usertype === 'freelance'){
+      navigation.navigate('Usercreate' as never)
+    } else {
+      ToastAndroid.show('Please choose one', ToastAndroid.SHORT)
+    }
+  }
 
   return (
     <ImageBackground source={background} resizeMode ='cover' style = {styles.container}>
-      <Text>Joinas</Text>
-      </ImageBackground>
+      <View style = {styles.h1container}>
+        <Text style = {styles.h1}>Join as an employer or Freelance</Text>
+      </View>
+      <JoinasButton
+        name = 'account-search-outline'
+        onPress={() =>{ setusertype('employer')}}
+        title = 'I want to hire'
+        style = {{borderColor: usertype === 'employer'? theme.accentd : black.B005, backgroundColor: usertype === 'employer' ? theme.accentc : theme.light}}
+        
+        status= {usertype === 'employer' ? 'checked' : 'unchecked'}
+        size = {25}
+        uncheckcolor= {black.B005}
+        radiocolor= {theme.accentd}
+      />
+      <JoinasButton
+        placeholder='I am a freelancer'
+        title = 'I am a freelancer'
+        name = 'account-search-outline'
+        onPress={() =>{setusertype('freelance')}}
+        size = {25}
+        status= {usertype === 'freelance' ? 'checked' : 'unchecked'}
+        style = {{borderColor: usertype === 'freelance' ? theme.accentd : black.B005, backgroundColor: usertype === 'freelance' ? theme.accentc : theme.light}}
+        radiocolor= {theme.accentd}
+      />
+      <View style = {{marginTop: 150, width: '100%', justifyContent: 'center', alignItems: 'center',}}>
+      <LogButton name = 'chevron-right' onPress={routechecking} title = 'Create Account'/>
+      <TextButton onPress={() => navigation.navigate('Login' as never)} text1='Already have an account?' text2=' Sign In here.'/>
+      </View>
+    </ImageBackground>
   )
 }
 
