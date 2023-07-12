63e1401110f13ef93442a60c69b742db373abd9e
Tordx <ellamateogang@gmail.com>
2023-07-13 03:12:20 +0800

    added layout tab navs

--- src/routes/layouts/index.tsx
+++ src/routes/layouts/index.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tab } from '../../../App'
-import Login from '../../screens/temp/login'
+import Home from '../../screens/home/freelance/home'
+import Notification from '../../screens/home/freelance/notification'
+import Saved from '../../screens/home/freelance/saved'
+import Account from '../../screens/home/freelance/account'
 
 
 const Tabs: React.FC = () => {
@@ -10,8 +13,29 @@ const Tabs: React.FC = () => {
 
    <Tab.Navigator>
       <Tab.Screen
-        name='Login'
-        component={Login}
+        name='Home'
+        component={Home}
+        options = {{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name='Notification'
+        component={Notification}
+        options = {{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name='Saved'
+        component={Saved}
+        options = {{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name='Account'
+        component={Account}
         options = {{
           headerShown: false,
         }}
