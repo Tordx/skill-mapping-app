f1b503482c7f565c2c1f4f07054a5b664d8d9781
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-12 22:18:29 +0800

    ...

--- src/screens/temp/usercreate.tsx
+++ src/screens/temp/usercreate.tsx
@@ -1,27 +1,30 @@
-import { View, Text, Image,Button, ScrollView } from 'react-native'
+import { View, Text, Image,Button, ScrollView, ToastAndroid} from 'react-native'
 import React, {useState} from 'react'
 import { styles } from '../../styles'
 import { Steps } from '../../global/functions'
-import Freelance from '../contents/signup/freelance'
+import { GoBack, LogButton, TextButton } from '../../global/partials/buttons'
+import { useNavigation } from '@react-navigation/native'
+import firestore from '@react-native-firebase/firestore'
+import { firebase } from '@react-native-firebase/auth'
+import { PersonalInfo } from '../contents/signup'
 
 type Props = {}
 const logo = require('../../assets/images/K6gggkO.png')
 
 const UserCreate = (props: Props) => {
 
-  const [position, setposition] = useState(0)
+  
+  const [position, setposition] = useState(0);
 
   return (
     <View style = {styles.container}>
       <ScrollView style = {styles.scrollview}>
-        <View style = {[styles.container, {paddingVertical: 50}]}>
-        <Image source={logo} resizeMode='contain' style = {{width: '50%', height: 200}}  />
+       <View style = {[styles.container, {paddingVertical: 75}]}>
+       {position <= 0 ? <Image source={logo} resizeMode='contain' style = {{width: '50%', height: 200}}  /> : <GoBack onPress={() => setposition(position - 1)}/>}
           <Steps
             currentPosition={position}
           />
-       
-          <Freelance/>
-          <Button title = 'press' onPress={() => setposition(position + 1)} />
+          <PersonalInfo position={position} setposition={setposition} />
         </View>
       </ScrollView>
     </View>
