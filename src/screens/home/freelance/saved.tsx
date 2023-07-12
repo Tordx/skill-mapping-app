9827eac13031fed5aa1787f63347de5c776d823d
Tordx <ellamateogang@gmail.com>
2023-07-13 03:13:14 +0800

    bottom tab screens

---
+++ src/screens/home/freelance/saved.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Saved = (props: Props) => {
+  return (
+    <View>
+      <Text>Saved</Text>
+    </View>
+  )
+}
+
+export default Saved
