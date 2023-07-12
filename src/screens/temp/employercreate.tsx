05b11b8d8dea1b904a1d93815bff4532988119ea
Tordx <ellamateogang@gmail.com>
2023-07-13 03:13:27 +0800

    added employer signup

--- src/screens/temp/employercreate.tsx
+++ src/screens/temp/employercreate.tsx
@@ -1,14 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, ScrollView, Image } from 'react-native'
+import React, { useState } from 'react'
+import { styles } from '../../styles'
+import { Steps } from '../../global/functions'
+import { EmployerInfo } from '../contents/signup'
+import { GoBack } from '../../global/partials/buttons'
 
 type Props = {}
-
+const logo = require('../../assets/images/K6gggkO.png')
 const Employercreate = (props: Props) => {
-  return (
-    <View>
-      <Text>Employercreate</Text>
-    </View>
-  )
-}
+    const [position, setposition] = useState(0);
+
+    return (
+      <View style = {styles.container}>
+        <ScrollView style = {styles.scrollview}>
+         <View style = {[styles.container, {paddingVertical: 75}]}>
+         {position <= 0 ? <Image source={logo} resizeMode='contain' style = {{width: '50%', height: 200}}  /> : <GoBack onPress={() => setposition(position - 1)}/>}
+            <Steps
+              currentPosition={position}
+              stepCount={3}
+            />
+            <EmployerInfo position={position} setposition={setposition} />
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
 
 export default Employercreate
