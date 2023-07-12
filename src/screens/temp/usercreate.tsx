6e0deec9ffe8296157cc568d50bbca1af3be07cf
Tordx <ellamateogang@gmail.com>
2023-07-12 11:37:53 +0800

    create user

--- src/screens/temp/usercreate.tsx
+++ src/screens/temp/usercreate.tsx
@@ -1,14 +1,29 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image,Button, ScrollView } from 'react-native'
+import React, {useState} from 'react'
 import { styles } from '../../styles'
+import { Steps } from '../../global/functions'
+import Freelance from '../contents/signup/freelance'
 
 type Props = {}
-const logo = require('../../assets/images/PgOLyqd.png')
+const logo = require('../../assets/images/K6gggkO.png')
 
 const UserCreate = (props: Props) => {
+
+  const [position, setposition] = useState(0)
+
   return (
     <View style = {styles.container}>
-      <Image source={logo} resizeMode='contain' style = {{width: '50%', height: 200}}  />
+      <ScrollView style = {styles.scrollview}>
+        <View style = {[styles.container, {paddingVertical: 50}]}>
+        <Image source={logo} resizeMode='contain' style = {{width: '50%', height: 200}}  />
+          <Steps
+            currentPosition={position}
+          />
+       
+          <Freelance/>
+          <Button title = 'press' onPress={() => setposition(position + 1)} />
+        </View>
+      </ScrollView>
     </View>
   )
 }
