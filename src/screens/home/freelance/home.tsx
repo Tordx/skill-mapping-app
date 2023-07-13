7141c0da5c1335719a0d76782f15ae763231f791
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-13 21:39:13 +0800

    job focus and other home info added

--- src/screens/home/freelance/home.tsx
+++ src/screens/home/freelance/home.tsx
@@ -1,12 +1,38 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { styles } from '../../../styles'
+import { SearchField } from '../../../global/partials/fields'
+import { useNavigation } from '@react-navigation/native'
+import { HoverButton } from '../../../global/partials/buttons'
+import { black, theme, white } from '../../../assets/colors'
+import JobsLists from '../../contents/home/freelance/home'
 
 type Props = {}
 
 const Home = (props: Props) => {
+
+  const [focus, setfocus] = useState(0)
+
+
+
+  const navigation = useNavigation()
   return (
-    <View>
-      <Text>Home</Text>
+    <View style = {styles.container}>
+      <SearchField onPress={() => navigation.navigate('Search' as never)}/>
+      <View style = {{flexDirection: 'row', marginTop: 15}}>
+        <HoverButton
+          onPress={() => setfocus(0)} 
+          title = 'Matched Jobs'
+          textStyle={{backgroundColor: focus  == 0 ? theme.primary : theme.light, color: focus == 0 ? white.main : black.main }}
+        />
+        <HoverButton 
+          onPress={() => setfocus(1)} 
+          title = 'All'
+          textStyle={{backgroundColor: focus  == 1 ? theme.primary : theme.light, color: focus == 1 ? white.main : black.main }}
+        
+        />
+      </View>
+      <JobsLists focus={focus} setfocus={setfocus}/>
     </View>
   )
 }
