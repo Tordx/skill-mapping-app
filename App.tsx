4cf3c9715a5d9861230b80b22781162d09280f95
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-13 21:35:35 +0800

    added provider inside app

--- App.tsx
+++ App.tsx
@@ -5,15 +5,18 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import Stacks from './src/routes/stack';
 import { theme } from './src/assets/colors';
-
+import {Provider} from 'react-redux'
+import store from './src/library/redux/store';
 export const Tab = createBottomTabNavigator();
 export const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
     <>
+      <Provider store={store}>
       <Stacks/>
       <StatusBar backgroundColor={theme.light} barStyle={'dark-content'} />
+      </Provider>
     </>
   )
 }
