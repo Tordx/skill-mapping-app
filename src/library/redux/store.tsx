a63c4a3b49f709e02edda8aa62ddb9107489f3f9
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-13 21:37:59 +0800

    redux for user info

---
+++ src/library/redux/store.tsx
@@ -0,0 +1,12 @@
+
+import {configureStore} from '@reduxjs/toolkit';
+import userslice from './userslice';
+
+
+export default configureStore({
+
+    reducer: {
+        _userdata : userslice
+    }
+    
+})
