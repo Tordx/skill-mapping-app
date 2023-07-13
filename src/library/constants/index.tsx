d249bfccde663a345b1ce314e5b9e1fbbe29a971
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-13 21:37:49 +0800

    centralized constants for g,obal interfaces

---
+++ src/library/constants/index.tsx
@@ -0,0 +1,44 @@
+export interface data {
+    firstname: string; 
+    middlename: string; 
+    lastname: string 
+    suffix: string;
+    dob: string;
+    gender: string;
+    nationality: string;
+    jobTitle: string;
+    highesteduc: string;
+    ProfLi: string;
+    Cert: string;
+    CSE: string;
+    SpeSkills: string;
+    Province: string;
+    City: string;
+    Barangay: string;
+    Street: string;
+    ContactNumber: string;
+    email: string;
+    emergencycontactname: string;
+    readonlyelationship: string;
+    emergencycontactnum: string;
+    address: string;
+    username: string;
+    fullname: string;
+    contactNumber: string;
+    website: string;
+  
+  }
+
+export interface jobdata {
+    jobtitle: string;
+    requirements: string;
+    type: string;
+    scope: string;
+    budget: string;
+    description: string;
+    jobid: string;
+    qualification: string;
+    userid: string;
+    fullname: string;
+
+}
