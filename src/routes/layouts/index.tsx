4c1d5559d4f0e526c962b151f0a96543e99ef399
Tordx <ellamateogang@gmail.com>
2023-07-17 03:33:57 +0800

    layouts added

--- src/routes/layouts/index.tsx
+++ src/routes/layouts/index.tsx
@@ -8,9 +8,14 @@ import Account from '../../screens/home/freelance/account'
 import { useFocusEffect } from '@react-navigation/native'
 import  Icon  from 'react-native-vector-icons/MaterialCommunityIcons'
 import { black, theme, transparent } from '../../assets/colors'
+import Employerhome from '../../screens/home/employer/home'
+import EmployerNotification from '../../screens/home/employer/notification'
+import EmployerSaved from '../../screens/home/employer/saved'
+import Employeraccount from '../../screens/home/employer/account'
+import Addjobs from '../../screens/home/employer/add'
 
 
-const Tabs: React.FC = () => {
+export const Tabs: React.FC = () => {
 
   useFocusEffect(() => {
     const exit = () =>{
@@ -108,4 +113,117 @@ const Tabs: React.FC = () => {
   )
 }
 
-export default Tabs
+export const EmployerTabs: React.FC = () => {
+
+  useFocusEffect(() => {
+    const exit = () =>{
+      BackHandler.exitApp()
+      return true
+    }
+    const handler = BackHandler.addEventListener('hardwareBackPress', exit)
+      return() => handler.remove()
+  })
+
+  return (
+
+   <Tab.Navigator
+   
+   screenOptions={({ }) => ({
+    headerShown: false,
+    tabBarShowLabel: false,
+    tabBarStyle: { borderColor: transparent.level01, height: 75, borderTopLeftRadius: 30, borderTopRightRadius: 30, position: 'absolute', bottom: 0},
+    tabBarIconStyle: { height: 20, justifyContent: 'center', alignItems: 'center' },
+  })}
+
+   >
+      <Tab.Screen
+        name='EmployerHome'
+        component={Employerhome}
+        options = {{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <>
+            <Icon
+            name = {focused ? 'home-variant' : 'home-variant-outline'}
+            color = {focused ? theme.accenta : black.B005}
+            size = {35}
+          />
+          <View style = {{marginTop: focused ? 10 : 0,borderTopWidth: focused ? 3 : 0, borderColor: theme.accenta, width: 20}}/>
+          </>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='EmployerNotification'
+        component={EmployerNotification}
+        options = {{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <>
+            <Icon
+            name = {focused ? 'bell' : 'bell-outline'}
+            color = {focused ? theme.accenta : black.B005}
+            size = {35}
+          />
+          <View style = {{marginTop: focused ? 10 : 0,borderTopWidth: focused ? 3 : 0, borderColor: theme.accenta, width: 20}}/>
+          </>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Addjobs'
+        component={Addjobs}
+        options = {{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <>
+            <Icon
+            name = {focused ? 'plus-box-outline' : 'plus-box'}
+            color = {theme.accenta}
+            size = {focused ? 45: 40}
+          />
+          <View style = {{marginTop: focused ? 10 : 0,borderTopWidth: focused ? 3 : 0, borderColor: theme.accenta, width: 20}}/>
+          </>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Employersave'
+        component={EmployerSaved}
+        options = {{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <>
+            <Icon
+            name = {focused ? 'book' : 'book-outline'}
+            color = {focused ? theme.accenta : black.B005}
+            size = {35}
+          />
+          <View style = {{marginTop: focused ? 10 : 0,borderTopWidth: focused ? 3 : 0, borderColor: theme.accenta, width: 20}}/>
+          </>
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Employeraccount'
+        component={Employeraccount}
+        options = {{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <>
+            <Icon
+            name = {focused ? 'account-circle' : 'account-circle-outline'}
+            color = {focused ? theme.accenta : black.B005}
+            size = {35}
+            />
+          <View style = {{marginTop: focused ? 10 : 0,borderTopWidth: focused ? 3 : 0, borderColor: theme.accenta, width: 20}}/>
+          </>
+          )
+        }}
+      />
+   </Tab.Navigator>
+
+
+
+  )
+}
