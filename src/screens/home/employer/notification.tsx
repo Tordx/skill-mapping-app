7cea1cf02850c2566d67a413d8cabe6c8e1bcc1a
Tordx <ellamateogang@gmail.com>
2023-07-17 03:35:00 +0800

    empnotif

---
+++ src/screens/home/employer/notification.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from '../../../styles'
+
+type Props = {}
+
+const EmployerNotification = (props: Props) => {
+  return (
+    <View style = {styles.container}>
+      <Text>EmployerNotification</Text>
+    </View>
+  )
+}
+
+export default EmployerNotification
---
+++ src/screens/home/employer/saved.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { styles } from '../../../styles'
+
+type Props = {}
+
+const EmployerSaved = (props: Props) => {
+  return (
+    <View style = {styles.container}>
+      <Text>EmployerSaved</Text>
+    </View>
+  )
+}
+
+export default EmployerSaved
