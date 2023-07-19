ddea49686f892efbb85fbf767d8d86a4d6189a55
Tordx <ellamateogang@gmail.com>
2023-07-19 21:57:12 +0800

    added functions and major fixes

--- src/screens/contents/home/employer/createjobs.tsx
+++ src/screens/contents/home/employer/createjobs.tsx
@@ -7,6 +7,9 @@ import { GoBack, JoinasButton, LogButton, NextButton, PrevButton } from '../../.
 import { Chip, RadioButton } from 'react-native-paper';
 import { firebase } from '@react-native-firebase/auth';
 import { getSpecificData } from '../../../../firebase';
+import firestore from '@react-native-firebase/firestore'
+import { useNavigation } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
   interface Props {
   position: number;
@@ -24,14 +27,11 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
     const [budget, setbudget] = useState(0);
     const [pertimeframe, setpertimeframe] = useState('');
     const [description, setdescription] = useState('');
-    const [jobid, setjobid] = useState('');
     const [qualification, setqualification] = useState('');
-    const [photourl, setphotourl] = useState('');
     const [userid, setuserid] = useState('');
     const [fullname, setfullname] = useState('');
     const [timestamp, settimestamp] = useState(null);
-    const [formattedtime, setformattedtime] = useState(null);
-
+    const navigation = useNavigation()
     const handleDelete = (index: number) => {
       const updatedRequirements = [...requirements];
       updatedRequirements.splice(index, 1);
@@ -59,7 +59,7 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
     
     const submit = () => {
       if(position === 8) {
-        // handleSave()
+        handleSave()
       } if (position === 7){
         if(!qualification) {
           ToastAndroid.show('Some fields might be blank', ToastAndroid.LONG)
@@ -76,11 +76,7 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
         if(!pertimeframe) {
           ToastAndroid.show('Some fields might be blank', ToastAndroid.LONG)
         } else {
-          if(!budget) {
-            ToastAndroid.show('Some fields might be blank', ToastAndroid.LONG)
-          } else {
           setposition(position + 1)
-        }
         }
       }  if (position === 4){
         if(!scope) {
@@ -109,66 +105,44 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
       }
     }
 
-    // const handleSave = async() => {
-    //   try {
-    //     await retrieveddata = await getSpecificData
-    //         const user = firebase.auth().currentUser;
-    //         const getid = firestore().collection('user').doc();
-    //         console.log(user);
-    //         firestore().collection('user').doc().set({
-    //           uid: getid,
-    //           fullname: [
-    //               {
-    //                 firstname: firstname,
-    //                 middlename: middlename,
-    //                 lastname: lastname,
-    //                 suffix: suffix,
-    //               }
-    //           ],
-    //           usertype: 'freelance',
-    //           photoURL: 'https://www.pngkey.com/png/full/202-2024792_user-profile-icon-png-download-fa-user-circle.png',
-    //           email: email,
-    //           dob: dob,
-    //           gender: gender,
-    //           nationality: nationality,
-    //           jobTitle: jobTitle,
-    //           highesteduc: highesteduc,
-    //           ProfLi: ProfLi,
-    //           Cert: Cert,
-    //           CSE: CSE,
-    //           SpeSkills: SpeSkills,
-    //           emergencycontactname: emergencycontactname,
-    //           emergencycontactnum: emergencycontactnum,
-    //           address: address,
-    //           readonlyelationship: readonlyelationship,
-    //           Address: [
-    //             {
-    //               Province: Province,
-    //               City: City,
-    //               Barangay: Barangay,
-    //               Street: Street,
-    //               ContactNumber: ContactNumber,
-    //             },
-    //           ],
-    //         });
-    //       }).then(async(response) => {
-    //         const user = firebase.auth().currentUser
-    //         user?.sendEmailVerification().then(() => {
-    //           navigation.navigate('Verification' as never)
-    //         })
-    //       });
-    //     });
-    //   } catch (error) {
-    //     console.log(`error: ${error}`);
-    //   }
-    // }
+    const handleSave = async() => {
+      try {
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+            const user = firebase.auth().currentUser;
+            const getid = firestore().collection('job-post').doc();
+            console.log(user);
+            await firestore().collection('job-post').doc().set({
+              jobtitle: jobtitle,
+              joblocation: joblocation,
+              requirements: requirements,
+              type: type,
+              scope: scope,
+              budget: budget,
+              pertimeframe: pertimeframe,
+              description: description,
+              jobid: getid,
+              qualification: qualification,
+              photoURL: user?.photoURL,
+              userid: user?.uid,
+              fullname: user?.displayName,
+              timestamp: timestamp,
+              status: true,
+          }).then(async(response) => {
+              ToastAndroid.show('HELLO', ToastAndroid.LONG)
+              setposition(position = 1)
+          });
+      } catch (error) {
+        console.log(`error: ${error}`);
+      }
+    }
   
 
 
   return (
-   <>
+    
+    <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
     {position == 1 && 
-      <>
+       <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
         <Text style={[styles.h1, {width: '95%'}]}>
           Write a title for your job post
         </Text>
@@ -186,10 +160,10 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
         <Text style = {{textAlign: 'left', fontSize: 12, width: '90%', fontFamily: 'Montserrat-Regular', marginBottom: 15, color: black.main}}>• Graphic Designer needed to design ads for multiple projects</Text>
         <Text style = {{textAlign: 'left', fontSize: 12, width: '90%', fontFamily: 'Montserrat-Regular', marginBottom: 15, color: black.main}}>• Facebook manager specialist needed for business</Text>
         <NextButton onPress={submit}/>
-        </>
+        </View>
     }
     {position == 2 && 
-      <>
+      <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
         <Text style={[styles.h1, {width: '95%', marginBottom: 30}]}>
          What are the main skills required for your work?
         </Text>
@@ -212,15 +186,15 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
                     >{requirement}</Chip>
                   ))}
         </View>
-        <>
-       <NextButton onPress={submit}/>
+        <View style = {{width: '90%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 25}}>
        <PrevButton onPress={prevpos}/>
-       </>
-        </>
+       <NextButton onPress={submit}/>
+       </View>  
+        </View>
     }
     {position == 3 && 
-      <>
-        <Text style={[styles.h1, {width: '90%', marginBottom: 30}]}>
+        <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
+        <Text style={[styles.h1, {width: '90%', marginTop: 30}]}>
          Next, Estimate the scope of your work
         </Text>
         <Text style = {{textAlign: 'left', fontSize: 16, width: '90%', fontFamily: 'Montserrat-Regular', marginTop: 50, color: black.main}}>This helps your job stand out to the right candidates.</Text>
@@ -260,15 +234,25 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
             <Text style = {{textAlign: 'left', fontSize: 12, width: '95%', fontFamily: 'Montserrat-Regular', color: black.main}}>Temporary employment in which the person works during certain times of the year</Text>
           </View>
         </View>
-        <>
-       <NextButton onPress={submit}/>
+        <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', color: black.main, marginTop: 35}}>Job Location</Text>
+        <DefaultField
+          placeholder="Work Address / Remote "
+          placeholderTextColor={black.B005}
+          size={25}
+          color={black.B004}
+          value = {joblocation}
+          onChangeText={(e) => setjoblocation(e)}
+          onKeyPress={(e) => handleKeyPress(e)}
+        />
+        <View style = {{width: '90%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 25}}>
        <PrevButton onPress={prevpos}/>
-       </>
-        </>
+       <NextButton onPress={submit}/>
+       </View>  
+        </View>
     }
     {position == 4 && 
-      <>
-        <Text style={[styles.h1, {width: '90%', marginBottom: 30}]}>
+       <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
+        <Text style={[styles.h1, {width: '90%', marginTop: 30}]}>
          Next, Estimate the scope of your work
         </Text>
         <Text style = {{textAlign: 'left', fontSize: 16, width: '90%', fontFamily: 'Montserrat-Regular', marginTop: 50, color: black.main}}>This helps your job stand out to the right candidates.</Text>
@@ -308,15 +292,15 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
             <Text style = {{textAlign: 'left', fontSize: 12, width: '95%', fontFamily: 'Montserrat-Regular', color: black.main}}>Longer term or complex initiative (ex. design and build a full website)</Text>
           </View>
         </View>
-        <>
-       <NextButton onPress={submit}/>
+        <View style = {{width: '90%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 25}}>
        <PrevButton onPress={prevpos}/>
-       </>
-        </>
+       <NextButton onPress={submit}/>
+       </View>  
+        </View>
     }
     {position == 5 && 
-      <>
-        <Text style={[styles.h1, {width: '90%', marginBottom: 30}]}>
+        <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
+        <Text style={[styles.h1, {width: '90%', marginTop: 30}]}>
           This will help us match you to talent within your range.
         </Text>
         <Text style = {{textAlign: 'left', fontSize: 16, width: '90%', fontFamily: 'Montserrat-Regular', marginTop: 50, color: black.main}}>This helps your job stand out to the right candidates.</Text>
@@ -335,8 +319,8 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
         name = 'currency-php'
         onPress={() =>{setpertimeframe('Project Budget')}}
         size = {20}
-        status= {pertimeframe === 'Project Base' ? 'checked' : 'unchecked'}
-        style = {{ borderColor: pertimeframe === 'Project Base' ? theme.accentd : black.B005, backgroundColor: pertimeframe === 'Project Base' ? theme.accentc : theme.light}}
+        status= {pertimeframe === 'Project Budget' ? 'checked' : 'unchecked'}
+        style = {{ borderColor: pertimeframe === 'Project Budget' ? theme.accentd : black.B005, backgroundColor: pertimeframe === 'Project Budget' ? theme.accentc : theme.light}}
         radiocolor= {theme.accentd}
         />
           <JoinasButton
@@ -359,49 +343,64 @@ const Createjobs: React.FC<Props> = ({position, setposition}) => {
         </View>
          
         }
-         <>
-       <NextButton onPress={submit}/>
+         <View style = {{width: '90%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 25}}>
        <PrevButton onPress={prevpos}/>
-       </>
-        </>
+       <NextButton onPress={submit}/>
+       </View>  
+        </View>
     }
     {position == 6 && 
-      <>
-        <Text style={[styles.h1, {width: '90%', marginBottom: 30}]}>
+        <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
+        <Text style={[styles.h1, {width: '90%', marginTop: 30}]}>
           Tell us about the project.
         </Text>
         <Text style = {{textAlign: 'left', fontSize: 16, width: '90%', fontFamily: 'Montserrat-Regular', marginTop: 25, color: black.main}}>Clear expectations about your project or deliverables.</Text>
         <View style = {{flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%',marginTop: 25}}>
        <Multitextfield style={{height: 250, alignContent: 'flex-start'}} value={description} onChangeText={(e) => setdescription(e)}/>
        </View>
-       <>
-       <NextButton onPress={submit}/>
+       <View style = {{width: '90%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 25}}>
        <PrevButton onPress={prevpos}/>
-       </>
-        </>
+       <NextButton onPress={submit}/>
+       </View>  
+        </View>
     }
     {position == 7 && 
-      <>
-        <Text style={[styles.h1, {width: '90%', marginBottom: 30}]}>
+       <View style = {{width: '100%', justifyContent: 'center', alignItems: 'center',}}>
+        <Text style={[styles.h1, {width: '90%', marginTop: 30}]}>
         Last step, start the conversation.
         </Text>
         <Text style = {{textAlign: 'left', fontSize: 16, width: '90%', fontFamily: 'Montserrat-Regular', marginTop: 25, color: black.main}}>It's important to include the specific skills, experience, and education.</Text>
         <View style = {{flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%',marginTop: 25}}>
        <Multitextfield style={{height: 250, alignContent: 'flex-start'}} value={qualification} onChangeText={(e) => setqualification(e)}/>
        </View>
-       <>
-       <NextButton onPress={submit}/>
+       <View style = {{width: '90%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 25}}>
        <PrevButton onPress={prevpos}/>
-       </>
-        </>
+       <NextButton onPress={submit}/>
+       </View>  
+        </View>
     }
-    {position === 8 && <>
+    {position === 8 &&
+      <View style = {{width: '100%',  justifyContent: 'center', alignItems: 'center',}}>
+      <Text style={[styles.h1, {width: '90%', marginTop: 30}]}>Job Details</Text>
+      <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', marginTop: 20, marginBottom: 10, color: black.main}}>Title</Text>
+      <Text style = {{textAlign: 'left', fontSize: 15, width: '90%', fontFamily: 'Montserrat-Regular', marginBottom: 5, color: black.main}}>{jobtitle}</Text>
+      <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', marginTop: 10, marginBottom: 10, color: black.main}}>Budget</Text>
+      <Text style = {{textAlign: 'left', fontSize: 15, width: '90%', fontFamily: 'Montserrat-Regular', marginBottom: 5, color: black.main}}>{budget}</Text>
+      <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', marginTop: 10, marginBottom: 10, color: black.main}}>Employment Type</Text>
+      <Text style = {{textAlign: 'left', fontSize: 15, width: '90%', fontFamily: 'Montserrat-Regular', marginBottom: 5, color: black.main}}>{type}</Text>
+      <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', marginTop: 10, marginBottom: 10, color: black.main}}>Project Scope</Text>
+      <Text style = {{textAlign: 'left', fontSize: 15, width: '90%', fontFamily: 'Montserrat-Regular', marginBottom: 5, color: black.main}}>{scope}</Text>
+      <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', marginTop: 10, marginBottom: 10, color: black.main}}>Description</Text>
+      <Text style = {{textAlign: 'left', fontSize: 15, width: '95%', fontFamily: 'Montserrat-Regular', marginTop: 10, marginBottom: 10, color: black.main, padding: 20, borderWidth: .5, borderRadius: 5, paddingLeft: 20}}>{description}</Text>
+      <Text style = {{textAlign: 'left', fontSize: 17, width: '95%', fontFamily: 'Montserrat-Bold', marginTop: 10, marginBottom: 10, color: black.main,}}>Qualifications</Text>
+      <Text style = {{textAlign: 'left', fontSize: 15, width: '95%', fontFamily: 'Montserrat-Regular', marginTop: 10, marginBottom: 10, color: black.main, padding: 20, borderWidth: .5, borderRadius: 5, paddingLeft: 20}}>{qualification}</Text>
+       
       <LogButton
             title='Post this Job'
             onPress={submit} 
           />
-          </>}
-   </>
+          </View>}
+          </View>
   )
 }
 
