02d2c6c0b6e7df278fb3689470bb42580cf1500f
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-22 22:53:32 +0800

    hover changes

--- src/screens/home/employer/home.tsx
+++ src/screens/home/employer/home.tsx
@@ -1,18 +1,17 @@
 import { View, Text } from 'react-native'
 import React, {useState} from 'react'
 import { styles } from '../../../styles'
-import PostList from '../../contents/home/employer/postlist'
 import { SearchField } from '../../../global/partials/fields'
 import { useNavigation } from '@react-navigation/native'
 import { HoverButton } from '../../../global/partials/buttons'
 import { black, theme, white } from '../../../assets/colors'
+import PostList from '../../contents/home/employer/posts/postlist'
 
 
 type Props = {}
 
 const Employerhome = (props: Props) => {
 
-  const [focus, setfocus] =  useState(0);
   const navigation = useNavigation()
 
   return (
@@ -20,19 +19,12 @@ const Employerhome = (props: Props) => {
       <SearchField onPress={() => navigation.navigate('Search' as never)}/>
       <View style = {{flexDirection: 'row', marginTop: 15,}}>
         <HoverButton
-          onPress={() => setfocus(0)} 
           title = 'My Posted Jobs'
-          textStyle={{backgroundColor: focus  == 0 ? theme.primary : theme.light, color: focus == 0 ? white.main : black.main }}
-        />
-        <HoverButton 
-          onPress={() => setfocus(1)} 
-          title = 'Archived'
-          textStyle={{backgroundColor: focus  == 1 ? theme.primary : theme.light, color: focus == 1 ? white.main : black.main }}
-        
+          textStyle={{backgroundColor: theme.primary, color:white.main}}
         />
         
       </View>
-      <PostList setfocus={setfocus} focus={focus}/>
+      <PostList/>
     </View>
   )
 }
