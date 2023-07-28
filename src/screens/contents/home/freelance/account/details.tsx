6391947c6d2ce8846b8f8280341d36d025874d46
Roven Kyle Narra <ellamateogang@gmail.com>
2023-07-28 21:02:51 +0800

    changes for info rendering

--- src/screens/contents/home/freelance/account/details.tsx
+++ src/screens/contents/home/freelance/account/details.tsx
@@ -1,9 +1,9 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Pressable, Modal } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import { styles } from '../../../../../styles'
 import { ScrollView } from 'react-native-gesture-handler'
 import { DefaultField, Multitextfield } from '../../../../../global/partials/fields'
-import { black, theme } from '../../../../../assets/colors'
+import { black, theme, white } from '../../../../../assets/colors'
 import { useSelector } from 'react-redux'
 import { data } from '../../../../../library/constants'
 import { GoBack } from '../../../../../global/partials/buttons'
@@ -15,40 +15,8 @@ type Props = {}
 const FPersonalInformation = (props: Props) => {
 
     const {userdata} = useSelector((action: data) => action._userdata);
-    const [detailaddress, setdetailedaddress] = useState('');
-    const [detailfullname, setdetailedfullname] = useState('');
-    const address = userdata[0]?.address;
-    const fullname = userdata[0]?.fullname;
+    const [openmodal, setopenmodal] = useState(false)
     const navigation = useNavigation()
-    
-    useEffect(() => {
-      // Combine the data into a single string
-      const combinedString = address
-        .map((item: any) => {
-          const key = Object.keys(item)[0];
-          const value = item[key];
-          return ` ${value}`;
-        })
-        .join(" ");
-  
-      // Save the combined string in the state
-      setdetailedaddress(combinedString);
-    }, []);
-    console.log(address[0]);
-
-    useEffect(() => {
-        // Combine the data into a single string
-        const combinedString = fullname
-          .map((item: any) => {
-            const key = Object.keys(item)[0];
-            const value = item[key];
-            return ` ${value}`;
-          })
-          .join(" ");
-    
-        // Save the combined string in the state
-        setdetailedfullname(combinedString);
-      }, []);
     
     
 
@@ -56,78 +24,82 @@ const FPersonalInformation = (props: Props) => {
     <View style = {styles.container}>
       <ScrollView style = {styles.scrollview}>
         <View style = {styles.container}>
-        <View style = {{width: 100, height: 100, borderColor: theme.primary, borderWidth: 5, borderRadius: 500, justifyContent: 'center', alignItems: 'center', marginTop: 75, marginBottom: 25}}>
+        <Pressable onPress={() => setopenmodal(true)} style = {{width: 100, height: 100, borderColor: theme.primary, borderWidth: 5, borderRadius: 500, justifyContent: 'center', alignItems: 'center', marginTop: 75, marginBottom: 25}}>
             <Image source={{uri: userdata[0].photoURL || 'https://i.imgur.com/AivI1mB.png'}} resizeMode='cover' style = {{width: '90%', height: '90%', borderRadius: 500}}/>
-          </View>
+          </Pressable>
         <Text style={[styles.h4, { fontFamily: 'Montserrat-SemiBold', marginBottom: 10 }]}>
              Personal Information
           </Text>
-          <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Name</Text>
+          <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>First Name</Text>
           <DefaultField
-              placeholder="Full name"
+              placeholder="First name"
               placeholderTextColor={black.B005}
               name="account-outline"
               size={25}
               color={black.B004}
-              value = {detailfullname}
+              value = {userdata[0].fullname[0].firstname}
               editable = {false}
           />
-        <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Contact Number</Text>
+           <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Middle Name</Text>
           <DefaultField
-              placeholder="contact number*"
+              placeholder="Middle name"
               placeholderTextColor={black.B005}
-              name="phone-outline"
+              name="account-outline"
               size={25}
               color={black.B004}
-              value = {userdata[0].contactnumber}
+              value = {userdata[0].fullname[0].middlename}
               editable = {false}
           />
-        <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Email</Text>
+           <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Last Name</Text>
+          <DefaultField
+              placeholder="Last name"
+              placeholderTextColor={black.B005}
+              name="account-outline"
+              size={25}
+              color={black.B004}
+              value = {userdata[0].fullname[0].lastname}
+              editable = {false}
+          />
+           <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Suffix</Text>
+          <DefaultField
+              placeholder="Suffix"
+              placeholderTextColor={black.B005}
+              name="account-outline"
+              size={25}
+              color={black.B004}
+              value = {userdata[0].fullname[0].suffix}
+              editable = {false}
+          />
+        <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Date of Birth</Text>
+          <DefaultField
+              placeholder="Birthdate"
+              placeholderTextColor={black.B005}
+              name="cake-variant-outline"
+              size={25}
+              color={black.B004}
+              value = {userdata[0].dob}
+              editable = {false}
+          />
+        <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Nationality</Text>
           <DefaultField
               placeholder="email"
               placeholderTextColor={black.B005}
-              name="email-outline"
+              name="flag-outline"
               size={25}
               color={black.B004}
-              value = {userdata[0].email}
+              value = {userdata[0].nationality}
               editable = {false}
           />
-        <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Address</Text>
-         <DefaultField
-              placeholder="Street"
-              placeholderTextColor={black.B005}
-              name="map-marker-outline"
-              size={25}
-              color={black.B004}
-              value = {detailaddress}
-              editable = {false}
-          />
-            <Text style = {{alignSelf: 'flex-start', marginLeft: 15, fontFamily: 'Montserrat-Regular', color: black.main, fontSize: 15}}>Address</Text>
-            <Multitextfield
-            style={{height: 250}}
-              placeholder="Enter your text here"
-              placeholderTextColor={black.B005}
-              name="calendar-outline"
-              size={25}
-              color={black.B004}
-              value = {userdata[0].description}
-              editable = {false}
-              
-          />
-             <DefaultField
-              placeholder="Business Hours"
-              placeholderTextColor={black.B005}
-              name="briefcase-outline"
-              size={25}
-              color={black.B004}
-              value = {userdata[0].businesshours}
-              editable = {false}
-          />
+       
           <View style={{ marginBottom: 50 }} />
           <GoBack onPress={() => navigation.goBack()}/>
         </View>
       </ScrollView>
-     
+      <Modal animationType='fade' visible = {openmodal} transparent statusBarTranslucent onRequestClose={() => setopenmodal(false)}>
+        <Pressable onPress={() => setopenmodal(false)} style = {{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: black.main}}>
+        <Image source={{uri: userdata[0].photoURL}} resizeMode='contain' style = {{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}}/>
+        </Pressable>
+      </Modal>
     </View>
   )
 }
